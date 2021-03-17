be5f25328ffe96477ca25f0bb3cc38dba67177f3
HoussamBedja <houssam.b@hotmail.com>
2021-03-16 20:43:31 -0400

    build dropdown with countries from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+          setCountries(countries);
+        })
+    }
+    getCountriesData();
+  }, [])
+
   return (
     <div className="app">
-      <h1>Covid 19 tracker</h1>
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__droopdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
